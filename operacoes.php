94981a3d98ad87808d154fb14d8cadaf8a84c71c
Enzo Nakahara <japanakah@outlook.com>
2022-11-22 18:31:15 -0300

    First commit

---
+++ operacoes.php
@@ -0,0 +1,11 @@
+<?php
+
+$soma = 10 + 10;
+$subtracao = 10 - 10;
+$divisao = 10 / 10;
+$multiplicacao = 10 * 10;
+$exponenciacao = 10 ** 10;
+$restoDeDivisao = 10 % 3;
+
+
+echo $exponenciacao;
---
+++ tipos.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 21;
+$salario = 1000.50;
+$divisao = 10 / 3;
+
+$texto = "Olá mundo";
+
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($falso);
