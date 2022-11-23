891b813c621ac343de131b19bb95d64609f96013
Enzo Nakahara <japanakah@outlook.com>
2022-11-23 08:04:07 -0300

    Aulas 5-1

--- primeiros-passos/decisoes.php
+++ primeiros-passos/decisoes.php
@@ -1,10 +1,17 @@
 <?php
 
-$idade = 18;
+$idade = 17;
+$numeroDePessoas = 1;
 
-echo "Você só pode entrar se tiver mais do que 18 anos." . PHP_EOL;
+echo "Você só pode entrar se tiver mais do que 18 anos, ou a partir de 16 anos se estiver acompanhado" . PHP_EOL;
 
 if ($idade >= 18) {
-    echo "Você tem $idade anos. " . PHP_EOL; 
-    echo "Pode entrar";
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} else if ($idade >= 16 && $numeroDePessoas > 1) {
+    echo "Você tem mais de 16 anos e está acompanhado(a)! Pode entrar.";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar.";
 };
+
+echo PHP_EOL;
+echo "Adeus!";
